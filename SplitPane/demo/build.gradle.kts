27d7cea418152c8a2d6570055a46f2a58ab1cf9d
Haneet Singh Chhabra <haneet555@gmail.com>
2022-06-13 01:33:55 +0530

    include HTTP

--- SplitPane/demo/build.gradle.kts
+++ SplitPane/demo/build.gradle.kts
@@ -15,11 +15,14 @@ kotlin {
                 implementation(compose.desktop.currentOs)
                 implementation(project(":SplitPane:library"))
                 implementation("io.coil-kt:coil-compose:1.4.0")
-
-                implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-                implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-                implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
-                implementation ("androidx.compose.runtime:runtime-livedata:1.1.0")
+                implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+                implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+                implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+                implementation("androidx.compose.runtime:runtime-livedata:1.1.0")
+                implementation("io.ktor:ktor-client-core:1.6.4")
+                implementation("io.ktor:ktor-client-cio:1.6.4")
+                implementation("io.ktor:ktor-client-serialization:1.6.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
             }
         }
     }
@@ -29,7 +32,11 @@ compose.desktop {
     application {
         mainClass = "org.jetbrains.compose.splitpane.demo.MainKt"
         nativeDistributions {
-            targetFormats(org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Exe)
+            targetFormats(
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg,
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi,
+                org.jetbrains.compose.desktop.application.dsl.TargetFormat.Exe
+            )
         }
 
     }
